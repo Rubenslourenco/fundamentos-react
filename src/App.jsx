4d3408417c3714b43d3d246d079a6fe1e21f0d15
Rubenslourenco <alvesrubens926@gmail.com>
2024-10-25 12:31:21 -0300

    funcões props, apagando tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -26,14 +26,36 @@ function App() {
     },
   ]);
 
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      //Preciso atualizar essa tarefa
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      //Não preciso atualizar essa tarefa
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="textl-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <Tasks tasks={tasks} />
         <AddTask />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
       </div>
     </div>
   );

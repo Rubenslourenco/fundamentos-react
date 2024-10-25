a8d526fbcaf4bd9927865155d81f8237b5ffaf3d
Rubenslourenco <alvesrubens926@gmail.com>
2024-10-25 17:42:21 -0300

    Atualizando projeto com o repositorio local

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,22 @@
 function AddTask() {
-  return <h1></h1>;
+  return (
+    <div className="space-y-4 bg-slate-400 rounded-md shadow flex flex-col">
+      <input
+        type="text"
+        placeholder="Digite o título da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      />
+      <input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      />
+
+      <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">
+        Adicionar
+      </button>
+    </div>
+  );
 }
 
 export default AddTask;
